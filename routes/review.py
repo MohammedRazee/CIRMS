48094327f7172730317348669ac471cc03098030
Mohammed Razee <razeeismail15@gmail.com>
2025-05-12 13:26:38 +0530

    Updating db from form

--- routes/review.py
+++ routes/review.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, Flask
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 from bson import ObjectId
 from routes.connection import mongo
 
@@ -14,3 +14,32 @@ def review(book_id):
     except Exception as e:
         print(f"Connection fail: {e}")
         return render_template("review.html", title="review_book.name", book=review_book , book_id=book_id)
+
+
+@review_bp.route("/submit/<book_id>", methods=['GET', 'POST'])
+def submit(book_id):
+    try:
+        email = request.form.get("email")
+        name = request.form.get("name")
+        review_msg = request.form.get("review")
+
+        # found_book = mongo.db.books.find_one({'_id': ObjectId(book_id)})
+        # found_review = mongo.db.reviews.find({'book_id': found_book['_id']})
+        # found_review = list(found_review)
+        
+        mongo.db.reviews.insert_one({'book_id': book_id, 'name': name, 'email': email, 'review': review_msg})
+        print("Connection successful in updating mongo")
+        return redirect(url_for('')) 
+    
+    # Continue heree, you were solving the routing problem
+    
+    except Exception as e:
+        print(f"Error detected: {e}")
+        return render_template("book.html", title="book.name")
+
+
+@review_bp.route("/check-email", methods=['POST'])
+def email_check():
+    email = request.json.get('email')
+    exists = mongo.db.users.find_one({'email': email})
+    return jsonify({'exists': bool(exists)})
