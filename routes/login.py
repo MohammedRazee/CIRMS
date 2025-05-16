1150d96975626c31522e712f1b275800e2fd3e13
Mohammed Razee <razeeismail15@gmail.com>
2025-05-16 10:52:24 +0530

    Login progress

--- routes/login.py
+++ routes/login.py
@@ -1,16 +1,49 @@
-from flask import render_template, Blueprint, json, request
+from flask import render_template, Blueprint, json, request, url_for, redirect, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from routes.connection import mongo
 
 
 login_bp = Blueprint("login", __name__)
 
-@login_bp.route("/register", methods=['GET', 'POST'])
+@login_bp.route("/register", methods=['GET'])
 def register():
-    name = request.json.get('name')
-    email = request.json.get('email')
-    pwd = request.json.get('password')
-    hashed_pwd = generate_password_hash(pwd)
-
-    mongo.db.users.insert_one({'name': name, 'email': email, 'pwd': hashed_pwd})
+    print("Registration page opened")
     return render_template("register.html", title="Login Page")
+
+
+@login_bp.route("/register", methods=['POST'])
+def get_register():
+    try:
+        name = request.form.get('name')
+        email = request.form.get('email')
+        pwd = request.form.get('password')
+        hashed_pwd = generate_password_hash(pwd)
+
+        mongo.db.users.insert_one({'name': name, 'email': email, 'pwd': hashed_pwd})
+        return redirect(url_for('home.home'))
+    
+    except Exception as e:
+        print(f"Registration error: {e}")
+        return redirect(url_for('home.home'))
+    
+@login_bp.route("/login", methods=['GET'])
+def login():
+    print("Login page loaded")
+    return render_template("login.html", title="Login Page")
+
+@login_bp.route("/login", methods=['POST'])
+def get_login():
+    email = request.json.get('email')
+    pwd = request.json.get('pwd')
+    user = mongo.db.users.find_one({'email': email})
+    if not user:
+        return jsonify({'exists': bool(user), 'pass': False})
+    
+    if not check_password_hash(user['pwd'], pwd):
+        return jsonify({'exists': False, 'pwd': True})
+    
+    return redirect(url_for('home.home'))
+    
+
+
+
